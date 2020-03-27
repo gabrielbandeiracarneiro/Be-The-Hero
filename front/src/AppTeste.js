7892ce03f0f43c49d562b6c21ed09614ccf2ef06
Gabriel Bandeira <gabrielbandeiracarneiro@gmail.com>
2020-03-26 22:29:01 -0300

    primeira rota

---
+++ front/src/AppTeste.js
@@ -0,0 +1,22 @@
+import React,{useState} from 'react';
+
+import Header from './Header';
+
+function App() {
+  let [counter,setCounter] = useState(0);
+
+  function increment(){
+    setCounter(counter+1);
+  }
+  return (
+    <div>
+      <Header >Contador: {counter}</Header>
+      <button onClick={increment}>Incrementar</button>
+    </div>
+  );
+}
+
+export default App;
+/*
+"emmet.syntaxProfiles": {"javascript":"jsx"},
+    "emmet.includeLanguages": {"javascript":"javascriptreact"}, */
